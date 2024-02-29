3b83b48ecc6bbeb61ccaa6456d1e482da918daba
Pragnesh <pragnesh.prajapati@weboccult.com>
2024-02-29 17:51:37 +0530

    edited index.js models file

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const { Client } = require("pg")
 const process = require('process');
 const basename = path.basename(__filename);
 const env = 'development';
@@ -10,8 +11,23 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+// sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+// Use the built-in Sequelize.postgres dialect
+sequelize = new Sequelize({
+  dialect: 'postgres',
+  database: config.database,
+  username: config.username,
+  password: config.password,
+  host: config.host,
+  port: config.port,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
 
 fs
   .readdirSync(__dirname)
