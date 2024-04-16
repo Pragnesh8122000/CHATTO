e81b51316b3d0ed9c9ae355f9f64ae41eb4a00db
Pragnesh <pragnesh.prajapati@weboccult.com>
2024-04-16 12:17:49 +0530

    removed mongoose from base controller

--- controller/base.controller.js
+++ controller/base.controller.js
@@ -1,7 +1,6 @@
 class BaseController {
     constructor() {
       this.jwt = require("jsonwebtoken");
-      this.mongoose = require("mongoose");
       this.helper = require("../helpers/helper");
       this.repo = require("../repo/repo");
       this.userServices = require("../services/user.services")
