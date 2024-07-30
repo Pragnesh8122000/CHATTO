4cccd7c02eb80e1e617c52f3835f31bbe65cd741
Jainil Alpeshbhai Solanki <jainilsolanki0704@gmail.com>
2024-07-30 09:55:42 +0530

    Added Anti Sleep

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketService = require('./socketServices');
+const request = require("request");
 
 const indexRouter = require("./routes/index");
 const middleware = require("./middleware/index");
@@ -31,6 +32,15 @@ app.use(
     indexRouter.privateRouter
 );
 
+function startAntiSleep(n){
+    const url = "https://chatto-be.onrender.com/api/conn";
+    request(url+'', (err,response,body) => {
+         console.log("Anti Sleep: ", n);
+        setTimeout(() => startAntiSleep(n+1), 1000*5*60);
+    })
+}
+
 server.listen(process.env.PORT || 3000, () => {
     console.log('Server listening on port:', process.env.PORT || 3000);
+    startAntiSleep(1);
 });
